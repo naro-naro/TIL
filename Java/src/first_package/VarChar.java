f9bb2ed9d4cc3a8d1587e157365dfb8333063ceb
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-18 13:32:12 +0900

    Update VarChar.java

--- Java/src/first_package/VarChar.java
+++ Java/src/first_package/VarChar.java
@@ -7,15 +7,13 @@ public class VarChar {
 		
 		char ch1, ch2, ch3, ch4;
 		String str1 = "abcd";	// "String"
-		ch1 = 'A';				// 'char'
+		ch1 = 'A';		// 'char'
 		ch2 = '1';
-		ch3 = 'ª¢';
-		ch4 = str1.charAt(1);	// StringúşÙşí®Öª"abcd"ªÎ1ÛãÙÍªÎÙşí®'b'ªòõÎõó
+		ch3 = str1.charAt(1);
 		
 		System.out.println("ch1 = " + ch1);
 		System.out.println("ch2 = " + ch2);
 		System.out.println("ch3 = " + ch3);
-		System.out.println("ch4 = " + ch4);
 		
 	}
 

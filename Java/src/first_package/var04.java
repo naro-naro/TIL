aa2fc2a8a7342e7e77149fbb1594bd0d27fa77a1
naro-naro <155498410+naro-naro@users.noreply.github.com>
2024-01-18 13:27:51 +0900

    Update var04.java

--- Java/src/first_package/var04.java
+++ Java/src/first_package/var04.java
@@ -1,11 +1,10 @@
-package ch04;
+package first_package;
 
 public class var04 {
 
 	public static void main(String[] args) {
 		char ch;
 		ch = 'A';
-		//char 변수는 작은따옴표!!!!!!!!!!!!!!!!!!
 		System.out.println(ch);
 
 	}

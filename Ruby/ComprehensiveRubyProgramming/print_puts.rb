56280e6075682badc4fefc65355c49fdf1074333
SiyeonPark <ccusy@naver.com>
2024-01-25 20:37:26 +0900

    FEAT: Ruby 폴더 생성 및 공부 파일 업로드
    Add TIL/Ruby
    Add hello.rb
    Add print_puts.rb
    Add puts_triangle.rb

---
+++ Ruby/ComprehensiveRubyProgramming/print_puts.rb
@@ -0,0 +1,14 @@
+print "Giraffe"
+puts "Elephant"
+
+puts "Elephant"
+print "Giraffe"
+
+puts "Elephant"
+puts " is big"
+
+print "Giraffe"
+print " is tall"
+
+# puts : prints on the different(new) line
+# print : prints on the same line

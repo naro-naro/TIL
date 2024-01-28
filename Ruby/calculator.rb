f7232df69db7b5fc612b7da68983e8209f9f6652
SiyeonPark <ccusy@naver.com>
2024-01-28 23:39:56 +0900

    FEAT: Ruby 공부 파일 수정 및 업로드
    Update input.rb
    Add calculator.rb

---
+++ Ruby/calculator.rb
@@ -0,0 +1,17 @@
+puts "Enter a number: "
+num1 = gets.chomp()
+puts "Enter another number: "
+num2 = gets.chomp()
+
+# puts (num1 + num2) : String(5) + String (2) = String(52)
+# puts (num1.to_i + num2.to_i) # .to_i : convert into integer
+puts (num1.to_f + num2.to_f) # .to_f : convert into float
+
+# ==
+
+# puts "Enter a number: "
+# num1 = gets.chomp().to_f # .to_f : convert into float
+# puts "Enter another number: "
+# num2 = gets.chomp().to_f  # .to_f : convert into float
+
+# puts (num1 + num2) 
--- Ruby/input.rb
+++ Ruby/input.rb
@@ -1,3 +1,12 @@
+# puts "Enter Your Name: "
+# name = gets
+# puts ("Hello " + name + ", you are cool!") # 내가 이름을 입력하고 enter를 눌렀기 때문에 new line에서 ", you are cool!"이 출력된다
+# # 실행 : 터미널에 .\input.rb 입력
+
+
 puts "Enter Your Name: "
-name = gets
-put ("Hello " + name)
+name = gets.chomp()
+puts "Enter Your Age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age) # .chomp() : 기존의 line에서 ", you are cool!"이 출력된다
+# 실행 : 터미널에 .\input.rb 입력
